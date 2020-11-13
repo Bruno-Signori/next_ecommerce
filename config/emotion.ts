7e148e25187d0fe45177b165012e37fc4ceb832a
Bruno-Signori <brunowsignori@gmail.com>
2020-11-12 21:24:29 -0300

    'teste'

---
+++ config/emotion.ts
@@ -0,0 +1,8 @@
+import styled, {CreateStyled} from '@emotion/styled';
+
+import theme from '../styles/theme';
+
+
+export default styled as CreateStyled<typeof theme>;
+
+// esse documneto é feito para iserrirmos as nossas configuraçoes
